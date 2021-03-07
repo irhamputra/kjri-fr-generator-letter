b1d078ea3fa4f3f0d77f9f4cd832538db6f5bb17
Muhamad Irham Prasetyo <muhamad.prasetyo@get-ag.com>
2021-03-07 22:00:43 +0100

    fix: implement admin frontend and backend, refactor file name

---
+++ pages/api/v1/admin.ts
@@ -0,0 +1,63 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { db } from "../../../utils/firebase";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  /**
+   * Admin Settings User
+   * @method POST
+   * @body email & password
+   * @return AuthResponse
+   */
+
+  if (req.method === "GET") {
+    let id = "";
+
+    try {
+      const snapshot = await db.collection("Admin").get();
+
+      snapshot.forEach((doc) => {
+        id = doc.id;
+      });
+
+      const doc = await db.collection("Admin").doc(id).get();
+
+      res.status(200).json(doc.data());
+      res.end();
+    } catch (e) {
+      res.status(500).json({ message: e });
+      res.end();
+    }
+  }
+
+  if (req.method === "POST") {
+    const { toggle } = req.body;
+
+    let id = "";
+
+    try {
+      const snapshot = await db
+        .collection("Admin")
+        .where("showRegisterForm", "!=", toggle)
+        .get();
+
+      if (snapshot.empty) {
+        res.status(400).json({ message: "Pengaturan tidak berubah" });
+      } else {
+        snapshot.forEach((doc) => {
+          id = doc.id;
+        });
+
+        await db.collection("Admin").doc(id).update({
+          showRegisterForm: toggle,
+        });
+
+        res.status(200).json({ message: "Pengaturan tersimpan" });
+        res.end();
+      }
+      res.end();
+    } catch (e) {
+      res.status(500).json({ message: e });
+      res.end();
+    }
+  }
+};
