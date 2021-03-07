1ccb9e84ce9e18d0cc1a654549e11022eec93f12
Muhamad Irham Prasetyo <muhamad.prasetyo@get-ag.com>
2021-03-07 09:27:28 +0100

    feat: add firebase admin

---
+++ utils/firebase/index.ts
@@ -0,0 +1,24 @@
+import admin from "firebase-admin";
+import { serviceAccount } from "./serviceAccount";
+
+if (!admin.app.length) {
+  try {
+    admin.initializeApp({
+      credential: admin.credential.cert({
+        projectId: serviceAccount.project_id,
+        privateKey: serviceAccount.private_key,
+        clientEmail: serviceAccount.client_email,
+      }),
+      projectId: serviceAccount.project_id,
+      storageBucket: "kjri-fr-dev.appspot.com",
+    });
+  } catch (error) {
+    console.log("Firebase admin initialization error", error.stack);
+  }
+}
+
+const db = admin.firestore();
+const auth = admin.auth();
+const storage = admin.storage();
+
+export { db, auth, storage };
