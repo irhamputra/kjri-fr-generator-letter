dfcf03e1e84f5effc8eaf28778962cfc28b77de7
Muhamad Irham Prasetyo <muhamad.prasetyo@get-ag.com>
2021-03-19 23:30:18 +0100

    fix: validatiob

--- hooks/useValidation.ts
+++ hooks/useValidation.ts
@@ -2,8 +2,7 @@ import { object } from "yup";
 import createSchema from "../utils/validation/schema";
 
 const useValidation = (initialValues: { [k: string]: string }) => {
-  const fieldNames = Object.keys(initialValues);
-  return object().shape(createSchema(fieldNames));
+  return object().shape(createSchema(initialValues));
 };
 
 export default useValidation;
