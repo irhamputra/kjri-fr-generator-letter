c2b1850c272a033ae15d5c3bc19b82433b67c88d
Abui <adjiem31@gmail.com>
2021-03-21 00:13:48 +0700

    refactor(Penugasan): change button tambah petugas

--- components/CustomField.tsx
+++ components/CustomField.tsx
@@ -1,5 +1,7 @@
+import { name } from "dayjs/locale/*";
 import * as React from "react";
-import Select from "react-select";
+import Select, { components, SelectComponentsConfig } from "react-select";
+import { Props } from "react-select/src/Select";
 
 const InputComponent: React.FC = (props) => {
   return (
@@ -12,12 +14,23 @@ const InputComponent: React.FC = (props) => {
   );
 };
 
-const SelectComponent = ({
+const SelectComponent: React.FC<Props> = ({
   placeholder,
   field,
   form: { setFieldValue, setFieldTouched, ...restForm },
   options,
+  components,
+  value,
+  styles,
+  matcher,
 }) => {
+  // reconstruct option from value
+  const index = matcher
+    ? matcher(options)
+    : options?.findIndex((valueProp) => valueProp === value);
+
+  const option = options?.[index];
+
   const handleOnChange = (option, { action }) => {
     if (action === "select-option") {
       setFieldValue(field.name, option?.value);
@@ -26,22 +39,70 @@ const SelectComponent = ({
 
   const handleOnFocus = () => {
     setFieldTouched(field.name, true);
-    console.log(restForm);
   };
 
   return (
     <div>
       <Select
         id={field.name}
+        value={value ? option : value}
         instanceId={field.name}
         isClearable
         placeholder={placeholder}
+        components={components}
         onChange={handleOnChange}
         options={options}
         onFocus={handleOnFocus}
+        styles={styles}
       />
     </div>
   );
 };
 
-export { InputComponent, SelectComponent };
+const mockStaff = [
+  {
+    name: "Acep Somantri",
+    nip: 1908283909123819,
+    golongan: "IV/c",
+    jabatan: "Kepala Perwakilan",
+    bidang: "Konsul Jendral",
+  },
+];
+
+const mockOptions = mockStaff.map(({ name, ...rest }) => ({
+  label: name,
+  value: ({ name, ...rest } as unknown) as string,
+}));
+
+const SelectStaff = ({ placeholder, form, field, value }) => {
+  // reconstruct option from value
+
+  const SingleValue = ({ children, data, ...props }) => {
+    const { nip, golongan, bidang, jabatan } = value;
+
+    return (
+      <components.SingleValue {...props}>
+        <div>
+          <div>{children}</div>
+          <div style={{ fontSize: 10, color: "rgba(0, 0, 0, 0.6)" }}>
+            {`${nip} | Gol ${golongan} | ${jabatan} ${bidang}`}
+          </div>
+        </div>
+      </components.SingleValue>
+    );
+  };
+
+  return (
+    <SelectComponent
+      components={{ SingleValue }}
+      placeholder={placeholder}
+      form={form}
+      value={value}
+      field={field}
+      styles={{ control: (provided) => ({ ...provided, height: 66 }) }}
+      options={mockOptions}
+    />
+  );
+};
+
+export { InputComponent, SelectComponent, SelectStaff };
