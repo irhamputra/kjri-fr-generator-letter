15566b73fc3a98e143860ed2b5f62ebc8a120432
Muhamad Irham Prasetyo <muhamad.prasetyo@get-ag.com>
2021-03-22 03:39:50 +0100

    fix: remove edit

--- pages/pengaturan/manage-golongan.tsx
+++ pages/pengaturan/manage-golongan.tsx
@@ -4,56 +4,33 @@ import { useFormik } from "formik";
 import { object } from "yup";
 import createSchema from "../../utils/validation/schema";
 import useQueryJalDir from "../../hooks/query/useQueryJalDir";
-import axios from "axios";
 import { toast } from "react-hot-toast";
 import { NextSeo } from "next-seo";
 import useDeleteGolonganMutation from "../../hooks/mutation/useDeleteGolonganMutation";
-import useEditGolonganMutation from "../../hooks/mutation/useEditGolonganMutation";
 import useCreateGolonganMutation from "../../hooks/mutation/useCreateGolonganMutation";
-import { v4 } from "uuid";
 
 const ManageGolongan: NextPage = () => {
-  const [status, setStatus] = React.useState("");
-
   const initialValues = {
     jenisGolongan: "",
     hargaGolongan: "",
     golId: "",
   };
 
-  const { mutateAsync: editGolongan } = useEditGolonganMutation<
-    typeof initialValues
-  >();
-
   const { mutateAsync: createGolongan } = useCreateGolonganMutation();
 
-  const {
-    handleChange,
-    handleSubmit,
-    values,
-    errors,
-    touched,
-    setFieldValue,
-  } = useFormik({
+  const { handleChange, handleSubmit, values, errors, touched } = useFormik({
     initialValues,
     validationSchema: object().shape(createSchema(initialValues)),
     onSubmit: async (values, { setSubmitting, resetForm }) => {
       setSubmitting(true);
 
       try {
-        if (status === "Edit") {
-          await editGolongan(values);
-        }
-
-        if (!status) {
-          await createGolongan(values);
-        }
+        await createGolongan(values);
       } catch (e) {
         toast.error("Terjadi kesalahan teknis! Mohon ulangi kembali");
         throw new Error(e.message);
       }
 
-      setStatus("");
       resetForm();
       setSubmitting(false);
     },
@@ -63,14 +40,6 @@ const ManageGolongan: NextPage = () => {
   const { data, isLoading } = useQueryJalDir();
 
   if (isLoading) return <h4>Loading...</h4>;
-
-  const handleEdit = async (id: string) => {
-    setStatus("Edit");
-    const { data } = await axios.get(`/api/v1/jaldir/${id}`);
-    await setFieldValue("jenisGolongan", data.golongan);
-    await setFieldValue("hargaGolongan", data.harga);
-    await setFieldValue("golId", id);
-  };
 
   const handleDelete = async (id: string) => {
     await deleteGolongan(id);
@@ -131,13 +100,6 @@ const ManageGolongan: NextPage = () => {
               <td scope="row">{v.golongan}</td>
               <td>$ {v.harga}</td>
               <td>
-                <button
-                  className="btn btn-primary"
-                  onClick={() => handleEdit(v.golId)}
-                >
-                  Edit
-                </button>
-
                 <button
                   className="btn btn-danger mx-3"
                   onClick={() => handleDelete(v.golId)}
