3e724b838e5aae36fc10ccabe409b76aa992bcb9
Abui <adjiem31@gmail.com>
2021-03-27 20:55:39 +0700

    add: role and disable tu access to manage-user

--- pages/api/v1/user/index.ts
+++ pages/api/v1/user/index.ts
@@ -3,22 +3,35 @@ import authInstance from "../../../../utils/firebase/authInstance";
 import { cors } from "../../../../utils/middlewares";
 import tokenInstance from "../../../../utils/firebase/tokenInstance";
 import qs from "qs";
+import { db } from "../../../../utils/firebase";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   await cors(req, res);
-  const adminList = ["tu@kjri-frankfurt.de", "admin@kjri-frankfurt.de"];
 
   if (req.method === "GET") {
     const token = req.headers.authorization.replace("Bearer ", "");
+
+    const adminRole = ["admin", "tu"];
 
     const { data } = await authInstance.post("/accounts:lookup", {
       idToken: token,
     });
 
     const [user] = data.users;
+
+    const snapshot = await db
+      .collection("Users")
+      .limit(1)
+      .where("email", "==", user.email)
+      .get();
+    const dataSnapshot = snapshot.docs[0].data();
+
     res
       .status(200)
-      .json({ email: user.email, isAdmin: adminList.includes(user.email) });
+      .json({
+        isAdmin: adminRole.includes(dataSnapshot.role),
+        ...dataSnapshot,
+      });
     res.end();
   }
 
