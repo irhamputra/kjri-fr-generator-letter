192e9a2768cf277e453d1038ff12dc4e94c621dd
Muhamad Irham Prasetyo <muhamad.prasetyo@get-ag.com>
2021-05-13 12:45:15 +0200

    refactor: remove react-dropzone

--- components/CustomField.tsx
+++ components/CustomField.tsx
@@ -1,84 +1,12 @@
-import { PropsWithChildren } from "react";
+import * as React from "react";
 import Select, { ActionMeta, components, SingleValueProps } from "react-select";
-import styles from "./Dropzone.module.css";
-import { useDropzone, FileWithPath, FileRejection, DropEvent, DropzoneProps } from "react-dropzone";
-import { FileEarmark as DocIcon } from "react-bootstrap-icons";
 import { FieldAttributes } from "formik";
-
-interface IUDropzone {
-  onDrop: <T extends File>(acceptedFiles: T[], fileRejections: FileRejection[], event: DropEvent) => void;
-  values: File[];
-  onClickReset: (event: unknown) => void;
-}
 
 interface InputProps extends React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> {
   endText: string;
 }
 
-const DropzoneComponent = ({
-  field,
-  form: { setFieldValue, setFieldTouched, ...restForm },
-}: PropsWithChildren<any>) => {
-  const onDrop = (acceptedFiles: File[]): void => {
-    setFieldValue(field.name, acceptedFiles);
-  };
-
-  const handleOnFocus = () => {
-    setFieldTouched(field.name, true);
-  };
-
-  const removeAll = () => {
-    setFieldValue(field.name, []);
-  };
-
-  return <UncontrolledDropzone values={field.value as Array<File>} onDrop={onDrop} onClickReset={() => removeAll()} />;
-};
-
-const UncontrolledDropzone = ({ onDrop, values = [], onClickReset }: IUDropzone): JSX.Element => {
-  const { getRootProps, getInputProps } = useDropzone({
-    onDrop,
-    accept: ["image/jpeg", "image/png", "application/pdf"],
-  });
-  const files = values.map((file: FileWithPath) => (
-    <div key={file.path}>
-      {file.path} - {file.size} bytes
-    </div>
-  ));
-
-  return (
-    <section style={{ background: "#f8f8f8" }} className="p-3 mb-3">
-      <div {...getRootProps({ className: styles.dropzone })}>
-        <input {...getInputProps()} />
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            flexDirection: "column",
-          }}
-        >
-          <DocIcon height={48} width={48} className="mb-3" style={{ color: "rgba(0,0,0,0.5)" }} />
-          <p>{files?.length > 0 ? files[0] : "File surat harus berekstensikan jpg, png atau pdf"}</p>
-        </div>
-      </div>
-      {files?.length > 0 && (
-        <div className="w-100 d-flex mt-1" style={{ justifyContent: "center", alignItems: "center" }}>
-          <small
-            onClick={onClickReset}
-            style={{
-              cursor: "pointer",
-            }}
-            className="m-0"
-          >
-            Pilih Ulang
-          </small>
-        </div>
-      )}
-    </section>
-  );
-};
-
-const InputComponent = ({ endText = "hari", ...props }: PropsWithChildren<InputProps>): JSX.Element => {
+const InputComponent = ({ endText = "hari", ...props }: React.PropsWithChildren<InputProps>): JSX.Element => {
   return (
     <div className="input-group">
       <input className="form-control" type="text" {...props} />
@@ -100,7 +28,7 @@ const SelectComponent = ({
   value,
   styles,
   matcher,
-}: PropsWithChildren<FieldAttributes<any>>): JSX.Element => {
+}: React.PropsWithChildren<FieldAttributes<any>>): JSX.Element => {
   // reconstruct option from value
   const index = matcher
     ? matcher(options)
@@ -138,7 +66,7 @@ const SelectComponent = ({
   );
 };
 
-const SelectStaff = ({ placeholder, form, field, value, options }: PropsWithChildren<FieldAttributes<any>>) => {
+const SelectStaff = ({ placeholder, form, field, value, options }: React.PropsWithChildren<FieldAttributes<any>>) => {
   // reconstruct option from value
   const optionStaff = options.map(({ displayName, ...rest }: { displayName: string }) => ({
     label: displayName,
@@ -176,4 +104,4 @@ const SelectStaff = ({ placeholder, form, field, value, options }: PropsWithChil
   );
 };
 
-export { InputComponent, SelectComponent, SelectStaff, DropzoneComponent, UncontrolledDropzone };
+export { InputComponent, SelectComponent, SelectStaff };
